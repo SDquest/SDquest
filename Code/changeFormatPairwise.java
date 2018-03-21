0c1a757a6af43617df83c54e892eff724c1bb004
SDquest <32338047+SDquest@users.noreply.github.com>
2018-03-21 09:30:59 +0800

    Add files via upload

--- Code/changeFormatPairwise.java
+++ Code/changeFormatPairwise.java
@@ -11,23 +11,35 @@ public class changeFormatPairwise {
 	public static int distance(String str, int start, int end){
 		int dis=0;
 		for(int i=start;i<end;i++){
-			if(str.charAt(i)!='N'){
+			if(str.charAt(i)!='N'&&str.charAt(i)!='n'){
 				dis++;
+			}
+		}
+		return dis;
+	}
+	public static int existN(String str){
+		int dis=0;
+		for(int i=0;i<str.length();i++){
+			if(str.charAt(i)=='N'||str.charAt(i)=='n'){
+				dis++;;
 			}
 		}
 		return dis;
 	}
 	public static void main (String args[]){
 		try{
+//			Scanner inChrN=new Scanner(new File("genome.txt"));
 			Scanner inChr=new Scanner(new File("genome.masked_all.fasta"));
 			Scanner in=new Scanner(new File("Genome_size_Indexes.txt")); String str; 
 			ArrayList<String> chrs=new ArrayList<String>();  boolean end=false;
 			ArrayList<String> segs=new ArrayList<String>();
+//			ArrayList<String> segsN=new ArrayList<String>();
 			while(in.hasNextLine()&&!end){
 				str=in.nextLine(); 
 				if(str.charAt(0)=='>'){
 					chrs.add(str.substring(1));
 					inChr.nextLine(); segs.add(inChr.nextLine());
+//					inChrN.nextLine(); segsN.add(inChrN.nextLine());
 				}
 				else{
 					end=true;
@@ -38,23 +50,63 @@ public class changeFormatPairwise {
 			
 			in=new Scanner(new File("SCN_LastzResult_500NonCR_NewExtendPec50.txt"));
 			BufferedWriter writer=new BufferedWriter (new FileWriter(new File("Pairwise_SDs.txt")));
-			in.nextLine(); String[] onepair; int indexW=0; double identity; int length1, length2;
-			writer.write("index  cigar  chrA  startA  endA  strandA  chrB  startB  endB  strandB  Match  Mismath  Indel  identity"); writer.newLine();
+			BufferedWriter writerBG=new BufferedWriter (new FileWriter(new File("BG_SDIndexes.fasta")));
+			in.nextLine(); String[] onepair; int indexW=0; int length1, length2; int start1,end1, start2,end2,chr, otherChr; int line=0, tempN1, tempN2;
+			writer.write("index  chrA  startA  endA  strandA  chrB  startB  endB  strandB  Match  Mismath  Indel  identity"); writer.newLine();
 			while(in.hasNextLine()){
-				onepair=in.nextLine().trim().split("[\\p{Space}]+");
-				if(Integer.parseInt(onepair[4])!=Integer.parseInt(onepair[7])||Integer.parseInt(onepair[5])>Integer.parseInt(onepair[9])||Integer.parseInt(onepair[8])>Integer.parseInt(onepair[6])){
-					identity=Double.parseDouble(onepair[10])/(Integer.parseInt(onepair[10])+Integer.parseInt(onepair[11])+Integer.parseInt(onepair[13]));
-					length1=distance(segs.get(Integer.parseInt(onepair[4])),Integer.parseInt(onepair[5]),Integer.parseInt(onepair[6]));
-					length2=distance(segs.get(Integer.parseInt(onepair[7])),Integer.parseInt(onepair[8]),Integer.parseInt(onepair[9]));
+				onepair=in.nextLine().trim().split("[\\p{Space}]+"); line++;
+				if(onepair[16].equalsIgnoreCase("-1")){
+					start1=Integer.parseInt(onepair[5]);
+				}
+				else{
+					start1=Integer.parseInt(onepair[16]);
+				}
+				if(onepair[17].equalsIgnoreCase("-1")){
+					end1=Integer.parseInt(onepair[6]);
+				}
+				else{
+					end1=Integer.parseInt(onepair[17]);
+				}
+				
+				if(onepair[18].equalsIgnoreCase("-1")){
+					start2=Integer.parseInt(onepair[8]);
+				}
+				else{
+					start2=Integer.parseInt(onepair[18]);
+				}
+				if(onepair[19].equalsIgnoreCase("-1")){
+					end2=Integer.parseInt(onepair[9]);
+				}
+				else{
+					end2=Integer.parseInt(onepair[19]);
+				}
+				chr=Integer.parseInt(onepair[4]); otherChr=Integer.parseInt(onepair[7]);
+				
+//				tempN1=existN(segsN.get(chr).substring(start1, end1)); tempN2=existN(segsN.get(otherChr).substring(start2, end2));
+//				if(tempN1>0||tempN2>0){
+//					System.out.println(line+"  "+tempN1+"  "+tempN2);
+//				}
+				
+				if(chr!=otherChr||start1>end2||start2>end1){
+					length1=distance(segs.get(chr),start1,end1);
+					length2=distance(segs.get(otherChr),start2,end2);
 					
-					if(identity>=0.7&&length1>=500&&length2>=500){
-						writer.write(indexW+"  "+onepair[1]+"  "+chrs.get(Integer.parseInt(onepair[4]))+"  "+onepair[5]+"  "+onepair[6]+"  "+onepair[14]+"  "+chrs.get(Integer.parseInt(onepair[7]))+"  "+onepair[8]+"  "+onepair[9]+"  "+onepair[15]+"  ");
-				        writer.write(onepair[10]+"  "+onepair[11]+"  "+onepair[13]+"  "+identity);
+					if(Double.parseDouble(onepair[22])>=0.7&&length1>=500&&length2>=500){
+						writer.write(indexW+"  "+chrs.get(chr)+"  "+start1+"  "+end1+"  "+onepair[14]+"  "+chrs.get(otherChr)+"  "+start2+"  "+end2+"  "+onepair[15]+"  ");
+				        writer.write(onepair[10]+"  "+onepair[11]+"  "+onepair[13]+"  "+onepair[22]);
 				        writer.newLine(); indexW++;
+				        writerBG.write(chr+"  "+start1+"  "+end1); writerBG.newLine();
+				        writerBG.write(otherChr+"  "+start2+"  "+end2); writerBG.newLine();
+					}
+					else{
+//						System.out.println("length and identity  "+line);
 					}
 				}
+				else{
+//					System.out.println("overlap  "+line);
+				}
 			}
-			in.close(); writer.close();
+			in.close(); writer.close(); writerBG.close();
 			System.out.println("Pairwise SDs can be found in Pairwise_SDs.txt!");
 		}catch (FileNotFoundException e) {
             e.printStackTrace();
